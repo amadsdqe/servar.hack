1b671f3829c426553246edd0a9176e754a17dcb2
amadsdqe <amadsdqe3@gmail.com>
2026-09-14 20:48:46 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -16,15 +15,16 @@ def login():
     password = request.form.get('password')
 
     if FAILED_ATTEMPTS >= 5:
-        return jsonify({"status": "blocked", "message": "ئایپی هاتیە قفلکرن! زێدەتر ژ ٥ هەوڵدانان کرینە."}), 429
+        return jsonify({"status": "blocked", "message": "Blocked IP!"}), 429
 
     if username == TARGET_USER and password == CORRECT_PASS:
         FAILED_ATTEMPTS = 0
-        return jsonify({"status": "success", "message": f"پاسوردێ درست هاتە دیتن: {password}"}), 200
+        msg = "Correct password: " + str(password)
+        return jsonify({"status": "success", "message": msg}), 200
     else:
         FAILED_ATTEMPTS += 1
-        return jsonify({"status": "failed", "message": "پاسورد شاشە!"}), 401
+        return jsonify({"status": "failed", "message": "Wrong password!"}), 401
 
 if __name__ == '__main__':
-    print("سێرڤەرێ لۆکال دەستبکار بوو ل سەر: http://127.0.0.1:5000")
+    print("Server running on port 5000...")
     app.run(port=5000)
